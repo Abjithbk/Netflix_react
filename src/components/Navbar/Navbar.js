83091766fd792c43367d2194ca9dc7c55b8a6db9
Abjithbk <bkabjith2@gmail.com>
2025-01-16 20:30:43 +0530

    Login clicking problem solved

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,7 +13,9 @@ function Navbar() {
       <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="" />
       <img className='Avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="" />
     <div className="buttons">
-     <span className='span1' >{user ? `Welcome ${user.displayName}`:'Login'}</span>
+     <span className='span1' onClick={()=> {
+      navigate('Login')
+     }} >{user ? `Welcome ${user.displayName}`:'Login'}</span>
      {user && <span className='span1' onClick={ async ()=> {
          await signOut(auth)
          navigate('/Login')
